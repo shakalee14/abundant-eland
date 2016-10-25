79bb622c7a99f54011d2923cbb45f6090aad4a3e
Max Antinone <mantinone@gmail.com>
2016-10-24 17:36:23 -0700

    login, logout, signup work

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var database = require('../database');
+
+/* GET home page. */
+router.get('/', function(request, response, next) {
+  response.render('api')
+  .catch( error => response.render('error', { error : error }));
+});
+
+router.post('/', function(request, response) {
+  database.createCustomer( request.body )
+  .then( customer => {
+    request.session.customerId = customer.id;
+    response.redirect('/');
+  })
+  .catch( error => response.render('error', { error : error }));
+})
+
+router.get('/logout', function(request, response) {
+  delete request.session.customerId;
+  response.render('logout')
+})
+
+router.post('/login', function(request, response) {
+  database.getCustomer( request.body )
+  .then( customer => {
+    request.session.customerId = customer.id;
+    response.redirect('/');
+  })
+  .catch( error => response.render('error', { error : error }));
+})
+
+module.exports = router;
