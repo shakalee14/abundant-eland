2409f32c6d705cc0154489677fd1d225f525212e
Max Antinone <mantinone@gmail.com>
2016-10-26 15:31:52 -0700

    finished transactions

--- database.js
+++ database.js
@@ -350,6 +350,7 @@ const getPizzaIngredients = attributes => {
 }
 
 
+
 const getAllTransactions = () => {
   const sql = `
     SELECT
@@ -358,6 +359,59 @@ const getAllTransactions = () => {
       transactions
   `
   return db.any( sql )
+}
+
+const createNewTransactionForCustomer = attributes => {
+  const sql = `
+    INSERT INTO transactions
+      ( customer_id, price_total, order_date )
+    VALUES
+      ( $1, $2, CURRENT_TIMESTAMP )
+    RETURNING
+     *
+  `
+  const variables = [ attributes.customer_id, attributes.price_total ]
+  return db.one( sql, variables )
+}
+
+const getMostRecentTransactionForCustomer = attributes => {
+  const sql = `
+    SELECT * FROM transactions
+    WHERE customer_id = $1
+    ORDER BY order_date DESC
+    LIMIT 1
+  `
+  return db.one( sql, [ attributes.id ])
+}
+
+const addDrinkToTransaction = (attributes, transaction_id) => {
+  const sql = `
+    INSERT INTO drink_transactions
+      ( drink_id, transaction_id, amount )
+    VALUES
+      ( $2, $1, $3),
+      ( $4, $1, $5)
+    RETURNING
+      *
+  `
+  const variables = [ transaction_id, attributes.drink_id1, attributes.drink_number, attributes.drink_id2, attributes.drink_number2 ]
+
+  return db.any( sql, variables )
+}
+
+const addPizzaToTransaction = ( attributes, transaction_id ) => {
+  const sql = `
+    INSERT INTO pizza_transactions
+      ( pizza_id, transaction_id, amount )
+    VALUES
+      ( $2, $1, $3),
+      ( $4, $1, $5)
+    RETURNING
+      *
+  `
+  const variables = [ transaction_id, attributes.pizza_id1, attributes.pizza_number, attributes.pizza_id2, attributes.pizza_number2 ]
+
+  return db.any( sql, variables )
 }
 
 const getTransactionById = attributes => {
@@ -371,6 +425,18 @@ const getTransactionById = attributes => {
     LIMIT 1
   `
   return db.one( sql, attributes.id )
+}
+
+const getTransactionsForCustomer = attributes => {
+  const sql = `
+    SELECT
+      *
+    FROM
+      transactions
+    WHERE
+      customer_id=$1
+  `
+  return db.any( sql, [attributes.id] )
 }
 
 const deleteTransaction = attributes => {
@@ -416,4 +482,9 @@ module.exports = {
   getTransactionById: getTransactionById,
   deleteTransaction: deleteTransaction,
   getPizzaIngredients: getPizzaIngredients,
+  createNewTransactionForCustomer: createNewTransactionForCustomer,
+  addPizzaToTransaction: addPizzaToTransaction,
+  addDrinkToTransaction: addDrinkToTransaction,
+  getTransactionsForCustomer, getTransactionsForCustomer,
+  getMostRecentTransactionForCustomer: getMostRecentTransactionForCustomer
 }
